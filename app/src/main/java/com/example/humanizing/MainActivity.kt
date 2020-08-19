495e473dfe2d8b0a93e0cfa8001c2b1ac3b26cd8
Andrey Malakhov <a.malakhov@home>
2020-08-19 18:03:10 +0300

    register callback

--- app/src/main/java/com/example/humanizing/MainActivity.kt
+++ app/src/main/java/com/example/humanizing/MainActivity.kt
@@ -1,6 +1,41 @@
 package com.example.humanizing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : RobotActivity() {
+
+	private lateinit var robotLifecycleCallbacks: RobotLifecycleCallbacks
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		registerRobotCallback()
+	}
+
+	override fun onDestroy() {
+		unregisterRobotCallback()
+		super.onDestroy()
+	}
+
+	private fun registerRobotCallback() {
+		robotLifecycleCallbacks = object : RobotLifecycleCallbacks {
+			override fun onRobotFocusGained(qiContext: QiContext) {
+			}
+
+			override fun onRobotFocusLost() {
+			}
+
+			override fun onRobotFocusRefused(reason: String?) {
+			}
+		}
+		QiSDK.register(this, robotLifecycleCallbacks)
+	}
+
+	private fun unregisterRobotCallback() {
+		QiSDK.unregister(this, robotLifecycleCallbacks)
+	}
 }
